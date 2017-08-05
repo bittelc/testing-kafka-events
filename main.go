fe0765b1eda7bb72e579fca9225790d3a1bb1cc7
Cole Bittel <cole.bittel@gmail.com>
2017-08-04 18:37:38 -0700

    Allow redis materialize

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import "github.com/go-redis/redis"
+
+var (
+	Redis = initRedis()
+)
+
+func initRedis() *redis.Client {
+	redisUrl := os.Getend("REDIS_URL")
+
+	if redisUrl == "" {
+		redisUrl = "127.0.0.1:6379"
+	}
+
+	return redis.NewClient(&redis.Options{
+		Addr:     redisUrl,
+		Password: "",
+		DB:       0,
+	})
+}
+
 func main() {
 }
